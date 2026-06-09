3bc695ae7e33db4f1a3a592ff912d5ac5ee588d8
tanishq13-tech <mahajantanishq70@gmail.com>
2026-06-09 15:08:08 +0530

    Add nested loop examples in Python for various patterns

---
+++ 9 June (Problems)/nestedLoop.py
@@ -0,0 +1,20 @@
+n = int(input("Enter the value of n: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(j, end=" ")
+    print("\n")
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*", end="")
+    print("\n")
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j, end="")
+    print("\n")
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i, end="")
+    print("\n")
