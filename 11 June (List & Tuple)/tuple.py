e954f030372391c31b1770e5bac62cc02cea33f0
tanishq13-tech <mahajantanishq70@gmail.com>
2026-06-12 12:51:28 +0530

    Add tuple operations examples in Python

---
+++ 11 June (List & Tuple)/tuple.py
@@ -0,0 +1,13 @@
+# Count
+t1= (1,2,3,2,2)
+print(t1.count(2))
+
+# Index
+print(t1.index(2))
+
+# Delete
+del t1
+print(t1)
+
+# Indexing & Slicing same as list
+# Append, remove, pop, insert like opertions not runn in tuple as tuple is immutable
