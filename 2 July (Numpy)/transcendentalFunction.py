ccba6e3cc35745ac46c4b8fdb3365c0f1d7e244b
tanishq13-tech <mahajantanishq70@gmail.com>
2026-07-02 18:52:03 +0530

    Add transcendentalFunction.py to demonstrate basic transcendental functions using NumPy

---
+++ 2 July (Numpy)/transcendentalFunction.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.arange(5)
+print(np.sin(a))
+print(np.cos(a))
+print(np.tan(a))
+print(1/np.sin(a))
+print(1/np.cos(a))
+print(1/np.tan(a))
+
+print(np.log(a))
+print(np.exp(a))
