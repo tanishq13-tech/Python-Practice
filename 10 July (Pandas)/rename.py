c8c000daa6e83ec51e8b364e4dc918f3c71fa2d5
tanishq13-tech <mahajantanishq70@gmail.com>
2026-07-10 20:13:37 +0530

    Add rename.py to demonstrate renaming columns in a DataFrame using Pandas

---
+++ 10 July (Pandas)/rename.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    "calories": [420, 380, 390, None],
+    "duration": [50, 40, 45, None],
+    "day": ["day1", "day2", "day3", None],
+    "year": [2020, 2021, 2022, None]
+}
+df = pd.DataFrame(data)
+print(df)
+df.rename(columns={"calories": "Calories", "duration": "Duration", "day": "Day", "year": "Year"}, inplace=True)
+print(df)
