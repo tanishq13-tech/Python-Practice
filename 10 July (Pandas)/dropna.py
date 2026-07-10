120e73e8704ddecc4d82205d22a6720d336f9b84
tanishq13-tech <mahajantanishq70@gmail.com>
2026-07-10 20:12:50 +0530

    Add dropna.py to demonstrate dropping missing values from DataFrames in Pandas

---
+++ 10 July (Pandas)/dropna.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+    "calories": [420, 380, 390, None],
+    "duration": [50, 40, 45, None],
+    "day": ["day1", "day2", "day3", None],
+    "year": [2020, 2021, 2022, None]
+}
+df = pd.DataFrame(data)
+print(df)
+ddf=df.dropna()
+print(ddf)
+
+myFile = pd.read_excel("10 July (Pandas)/sample-simple.xlsx", index_col=0)
+print(myFile)
+ddf=myFile.dropna()
+print(ddf)
